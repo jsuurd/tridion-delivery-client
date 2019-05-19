ebe2e0f2499060e5806f517fee19ed7063724453
Jeroen Suurd <jeroen.suurd@suurd.org>
2019-05-19 10:18:50 -0400

    Updated unit tests

--- tridion-content-client/src/test/java/org/suurd/tridion/content/client/ODataV2ContentServiceClientTests.java
+++ tridion-content-client/src/test/java/org/suurd/tridion/content/client/ODataV2ContentServiceClientTests.java
@@ -3,7 +3,6 @@ package org.suurd.tridion.content.client;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
-import java.text.ParseException;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -11,13 +10,13 @@ import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.suurd.tridion.content.client.model.Binary;
+import org.suurd.tridion.content.client.model.BinaryContent;
+import org.suurd.tridion.content.client.model.Component;
 import org.suurd.tridion.content.client.model.ComponentPresentation;
 import org.suurd.tridion.content.client.model.CustomMeta;
 import org.suurd.tridion.content.client.model.Keyword;
 import org.suurd.tridion.content.client.model.Page;
 import org.suurd.tridion.content.client.model.PageContent;
-
-import com.tridion.util.TCMURI;
 
 public class ODataV2ContentServiceClientTests {
 
@@ -30,64 +29,65 @@ public class ODataV2ContentServiceClientTests {
 
 	@Test
 	@Ignore("Integration Test")
-	public void getBinary_WithValidBinaryId_ShouldReturnBinary() throws ParseException {
-		//TCMURI binaryId = new TCMURI("tcm:5-272");
-		TCMURI binaryId = new TCMURI("tcm:5-81");
+	public void getBinary_WithValidIds_ShouldReturnBinary() {
+		int publicationId = 5;
+		int binaryId = 272;
 		
-		Binary binary = contentServiceClient.getBinary(binaryId);
+		Binary binary = contentServiceClient.getBinary(publicationId, binaryId);
+		
+		assertNotNull(binary);
+	}
+
+	@Test
+	 @Ignore("Integration Test")
+	public void getBinaryContent_WithValidIds_ShouldReturnBinaryContent() {
+		int publicationId = 5;
+		int binaryId = 272;
+		String variantId = "WyNkZWYjXQ==";
+		
+		BinaryContent binary = contentServiceClient.getBinaryContent(publicationId, binaryId, variantId);
 		
 		assertNotNull(binary);
 	}
 
 	@Test
 	@Ignore("Integration Test")
-	public void getComponentPresentation_WithValidComponentAndTemplateId_ShouldReturnComponentPresentation() throws ParseException {
-		TCMURI componentId = new TCMURI("tcm:5-256-16");
-		TCMURI templateId = new TCMURI("tcm:5-174-32");
+	public void getComponent_WithValidIds_ShouldReturnComponent() {
+		int publicationId = 5;
+		int componentId = 256;
 		
-		ComponentPresentation componentPresentation = contentServiceClient.getComponentPresentation(componentId, templateId);
+		Component component = contentServiceClient.getComponent(publicationId, componentId);
+		
+		assertNotNull(component);
+	}
+
+	@Test
+	@Ignore("Integration Test")
+	public void getComponentPresentation_WithValidIds_ShouldReturnComponentPresentation() {
+		int publicationId = 5;
+		int componentId = 256;
+		int templateId = 174;
+		
+		ComponentPresentation componentPresentation = contentServiceClient.getComponentPresentation(publicationId, componentId, templateId);
 		
 		assertNotNull(componentPresentation);
 	}
-	
+
 	@Test
 	@Ignore("Integration Test")
-	public void getCustomMetas () throws ParseException {
-		TCMURI itemId = new TCMURI("tcm:5-292");
+	public void getCustomMetas_WithValidIds_ShouldReturnCustomMetas () {
+		int publicationId = 5;
+		int itemId = 292;
 		
-		List<CustomMeta> customMetas = contentServiceClient.getCustomMetas(itemId);
-		
-		assertTrue(customMetas.size() > 0);
-	}
-	
-	@Test
-	@Ignore("Integration Test")
-	public void getCustomMetas2 () throws ParseException {
-		TCMURI publicationId = new TCMURI("tcm:0-5-1");
-		String keyName = "sitemapKeyword";
-		String stringValue = "000 Home";
-		
-		List<CustomMeta> customMetas = contentServiceClient.getCustomMetas(publicationId, keyName, stringValue);
-		
-		assertTrue(customMetas.size() > 0);
-	}
-	
-	@Test
-	@Ignore("Integration Test")
-	public void getCustomMetas3 () throws ParseException {
-		TCMURI publicationId = new TCMURI("tcm:0-5-1");
-		String keyName = "pageSize";
-		float floatValue = 5;
-		
-		List<CustomMeta> customMetas = contentServiceClient.getCustomMetas(publicationId, keyName, floatValue);
+		List<CustomMeta> customMetas = contentServiceClient.getCustomMetas(publicationId, itemId);
 		
 		assertTrue(customMetas.size() > 0);
 	}
 
 	@Test
 	@Ignore("Integration Test")
-	public void getCustomMetas4 () throws ParseException {
-		TCMURI publicationId = new TCMURI("tcm:0-5-1");
+	public void getCustomMetas_WithValidDateValue_ShouldReturnCustomMetas  () {
+		int publicationId = 5;
 		String keyName = "dateCreated";
 		LocalDateTime dateValue = LocalDateTime.parse("2014-08-31T10:00:00");
 		
@@ -98,41 +98,69 @@ public class ODataV2ContentServiceClientTests {
 
 	@Test
 	@Ignore("Integration Test")
-	public void getKeyword_WithValidKeywordIdAndTaxonomyId_ShouldReturnKeyword() throws ParseException {
-		TCMURI keywordId = new TCMURI("tcm:5-293-1025");
-		TCMURI taxonomyId = new TCMURI("tcm:5-47-512");
+	public void getCustomMetas_WithValidFloatValue_ShouldReturnCustomMetas () {
+		int publicationId = 5;
+		String keyName = "pageSize";
+		float floatValue = 5;
 		
-		Keyword keyword = contentServiceClient.getKeyword(keywordId, taxonomyId);
+		List<CustomMeta> customMetas = contentServiceClient.getCustomMetas(publicationId, keyName, floatValue);
+		
+		assertTrue(customMetas.size() > 0);
+	}
+
+	@Test
+	@Ignore("Integration Test")
+	public void getCustomMetas_WithValidStringValue_ShouldReturnCustomMetas () {
+		int publicationId = 5;
+		String keyName = "sitemapKeyword";
+		String stringValue = "000 Home";
+		
+		List<CustomMeta> customMetas = contentServiceClient.getCustomMetas(publicationId, keyName, stringValue);
+		
+		assertTrue(customMetas.size() > 0);
+	}
+
+	@Test
+	@Ignore("Integration Test")
+	public void getKeyword_WithValidIds_ShouldReturnKeyword() {
+		int publicationId = 5;
+		int taxonomyId = 47;
+		int keywordId = 293;
+		
+		Keyword keyword = contentServiceClient.getKeyword(publicationId, taxonomyId, keywordId);
 		
 		assertNotNull(keyword);
 	}
-	
+
 	@Test
 	@Ignore("Integration Test")
-	public void getKeywords_WithValidTaxonomyId_ShouldReturnKeywords() throws ParseException {
-		TCMURI taxonomyId = new TCMURI("tcm:5-47-512");
+	public void getKeywords_WithValidIds_ShouldReturnKeywords() {
+		int publicationId = 5;
+		int taxonomyId = 47;
 		
-		List<Keyword> keywords = contentServiceClient.getKeywords(taxonomyId);
+		List<Keyword> keywords = contentServiceClient.getKeywords(publicationId, taxonomyId);
 		
 		assertNotNull(keywords);
 	}
 
 	@Test
 	@Ignore("Integration Test")
-	public void getPage_WithValidPageId_ShouldReturnPage() throws ParseException {
-		TCMURI pageId = new TCMURI("tcm:5-247-64");
+	public void getPage_WithValidIds_ShouldReturnPage() {
+		int publicationId = 5;
+		int pageId = 247;
 		
-		Page page = contentServiceClient.getPage(pageId);
+		Page page = contentServiceClient.getPage(publicationId, pageId);
 		
 		assertNotNull(page);
 	}
 
 	@Test
 	@Ignore("Integration Test")
-	public void getPageContent_WithValidPageId_ShouldReturnPageContent() throws ParseException {
-		TCMURI pageId = new TCMURI("tcm:5-247-64");
+	public void getPageContent_WithValidIds_ShouldReturnPageContent() {
+		int publicationId = 5;
+		int pageId = 247;
 		
-		PageContent pageContent = contentServiceClient.getPageContent(pageId);
+		PageContent pageContent = contentServiceClient.getPageContent(publicationId, pageId);
 		
 		assertNotNull(pageContent);
 	}
